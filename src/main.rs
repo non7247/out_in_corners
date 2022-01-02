1153df23e4311fdb474109ffa804f7b5d4b82019
non7247 <funi_funi@nifty.com>
2022-01-02 13:27:06 +0900

    多角形内の点の内外判定(交差法)の関数を追加

--- src/main.rs
+++ src/main.rs
@@ -20,3 +20,33 @@ fn main() {
         println!("{:?} {:?}", l.start_point, l.end_point);
     }
 }
+
+fn is_inside(p: &geo::Point, curves: &Vec<geo::Line>) -> bool {
+    for l in curves.iter() {
+        if l.is_on(p, false, &Tolerance::default()) {
+            return true;
+        }
+    }
+
+    let tol = Tolerance::default().equal_point();
+    let mut cn = 0;
+
+    for l in curves.iter() {
+        let sy = l.start_point.y;
+        let ey = l.end_point.y;
+
+        if (sy + tol < p.y && ey + tol > p.y) ||
+                (sy + tol > p.y && ey - tol < p.y) {
+            let ray = geo::Line { start_point: *p,
+                                  end_point: *p + geo::Vector { x: 1.0, y: 0.0, z: 0.0 } };
+
+            if let Ok(ip) = l.intersect_with_line(&ray, true, &Tolerance::default()) {
+                if p.x < ip.x + tol {
+                    cn += 1;
+                }
+            }
+        }
+    }
+
+    cn % 2 == 1
+}
